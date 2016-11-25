137d7a428be2a0ff828691116d233945e5846c1f
Mia <mobrien@stuy.edu>
2016-11-24 22:12:36 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
+#include <ctype.h>
 
 int cd(char *pth){
     int BUFFERSIZE=50;
@@ -21,6 +23,26 @@ int cd(char *pth){
     return 0;
 }
 
+char *trim(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator
+  *(end+1) = 0;
+
+  return str;
+}
+
 int main() {
   while(1){
     char buf[50];
@@ -30,17 +52,60 @@ int main() {
     fgets(buf, sizeof(buf), stdin);
     char * bufadd = buf;
     bufadd = strsep(&bufadd, "\n");
-    char *command[10];
+    char *command[20];
 
-    int i = 0;
-    /* while (bufadd) {
-      command[i] = strsep(&bufadd, " ");
-      i++;
-    }*/
+    int i=0;
+    int first=1;
+    int out=0;
+    char *rest = buf;
    // *strchr(buf, "\n") = NULL;
+    char  *sin, *sout;
+    char  *fin, *fout;
+    int fdin=-1;
+    int fdout=-1;
+    
+    while ( (sin=strchr(rest, '<')) != NULL 
+      ||  (sout=strchr(rest, '>')) != NULL ) {     
+      //contains "<" or ">" 
+        if (first && sin != NULL && sout==NULL){
+        // only input
+            bufadd = strsep(&rest , "<");
+            fin = trim(rest);
+        }
+        if (first && sin == NULL && sout!=NULL){
+        // only output
+            bufadd = strsep(&rest , ">");
+            fout = trim(rest);   
+        }
+        if (sin != NULL && sout!=NULL)
+            bufadd = strsep(&rest , "<>");         
+        if (!first && sin != NULL && sout==NULL){
+        // > < 
+            fout = trim(strsep(&rest , "<"));
+            fin = trim(rest);
+        }
+        // < >
+        if ( !first && sin == NULL && sout!=NULL){
+            fin = strsep(&rest , ">");
+            fout = rest;            
+        }
+        first = 0;
+
+       if (fin != NULL) {
+            fdin = open(fin, O_RDONLY);
+       }
+       if (fout != NULL) {
+            fdout = open(fout, O_CREAT, 0666);
+            close(fdout);
+            fdout = open(fout, O_WRONLY);
+       }       
+    } 
+    
+    bufadd = trim(bufadd);
+    int saved_stdout;
+    int saved_stdin;
     for (i; command[i] = strsep(&bufadd, " "); i++);
     command[i] = 0;
-    
     if (!(strcmp(command[0],"exit"))){
       exit(0);
     }
@@ -50,12 +115,24 @@ int main() {
       strcpy(directories, command[1]);
       cd(directories);
     }    
-    else { 
+    else {
+        
+        if(fdin != -1){
+            saved_stdin = dup(0);
+            dup2(fdin, 0);
+        }
+        if(fdout!= -1) {
+            saved_stdout = dup(1);
+            dup2(fdout, 1);
+        }
+
         int pid = fork();
         if (pid == 0)
-           execvp( command[0], command );]
+           execvp( command[0], command );
         else
            wait(NULL);
+        dup2(saved_stdout, 1);
+        dup2(saved_stdin, 0);
    }
   }
 }
