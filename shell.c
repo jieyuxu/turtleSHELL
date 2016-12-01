3c2f6180d67ae7addf87da3486523a5002640e8f
jieyuxu <jieyu.stuy@gmail.com>
2016-11-30 21:42:17 -0500

    stuff to shell.c

--- shell.c
+++ shell.c
@@ -36,15 +36,55 @@ void rightRD(char * buf){
 /*
 REDIRECTION USING <
 */
-void leftRD(char * buf){
+void redirect(char * rest){
+  int first=1;
+  char *sin, *sout;
+  char *fin, *fout;
+  int fdin=-1;
+  int fdout=-1;
+  while ( (sin=strchr(rest, '<')) != NULL 
+    ||  (sout=strchr(rest, '>')) != NULL ) {     
+    //contains "<" or ">" 
+      if (first && sin != NULL && sout==NULL){
+      // only input
+          bufadd = strsep(&rest , "<");
+          fin = trim(rest);
+      }
+      if (first && sin == NULL && sout!=NULL){
+      // only output
+          bufadd = strsep(&rest , ">");
+          fout = trim(rest);   
+      }
+      if (sin != NULL && sout!=NULL)
+          bufadd = strsep(&rest , "<>");         
+      if (!first && sin != NULL && sout==NULL){
+      // > < 
+          fout = trim(strsep(&rest , "<"));
+          fin = trim(rest);
+      }
+      // < >
+      if ( !first && sin == NULL && sout!=NULL){
+          fin = strsep(&rest , ">");
+          fout = rest;            
+      }
+      first = 0;
 
+     if (fin != NULL) {
+          fdin = open(fin, O_RDONLY);
+     }
+     if (fout != NULL) {
+          fdout = open(fout, O_CREAT, 0666);
+          close(fdout);
+          fdout = open(fout, O_WRONLY);
+     }       
+  } 
 }
 
 /*
 PIPE
 */
 void pipe(char * buf){
-
+  
 }
 
 int main() {
