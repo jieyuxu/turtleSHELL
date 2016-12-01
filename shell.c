1f8cf00c8f8ee9288e1fe5c26ec45c31d1fabb5a
jieyuxu <jieyu.stuy@gmail.com>
2016-12-01 15:30:36 -0500

    remove comment

--- shell.c
+++ shell.c
@@ -23,68 +23,58 @@ REDIRECTION USING <
 */
 
 
-// int main() {
-//   while(1){
-//     char buf[50];
-//     char getcd[50];
-//     char hostname[50];
-//     if (gethostname(hostname, sizeof(hostname)) == -1)
-//       printf("%s ", getcwd(getcd, sizeof(getcd)));
-//     else
-//       printf("%s %s$ ", getcwd(getcd, sizeof(getcd)), getenv("USER"));
-//     //  printf("Enter commands separated by a single space: "); 
-
-//     int i=0;
-//     int first=1;
-//     char *rest = buf;
-//    // *strchr(buf, "\n") = NULL;
-//     char  *sin, *sout;
-//     char  *fin, *fout;
-//     int fdin=-1;
-//     int fdout=-1;
+int redirect() {
+    int i=0;
+    int first=1;
+    char *rest = buf;
+   // *strchr(buf, "\n") = NULL;
+    char  *sin, *sout;
+    char  *fin, *fout;
+    int fdin=-1;
+    int fdout=-1;
     
-//     while ( (sin=strchr(rest, '<')) != NULL 
-//       ||  (sout=strchr(rest, '>')) != NULL ) {     
-//       //contains "<" or ">" 
-//         if (first && sin != NULL && sout==NULL){
-//         // only input
-//             bufadd = strsep(&rest , "<");
-//             trim(rest);
-//             fin = rest;
-//         }
-//         if (first && sin == NULL && sout!=NULL){
-//         // only output
-//             bufadd = strsep(&rest , ">");
-//             trim(rest);
-//             fout = rest;
-//         }
-//         if (sin != NULL && sout!=NULL)
-//             bufadd = strsep(&rest , "<>");         
-//         if (!first && sin != NULL && sout==NULL){
-//         // > < 
-//             fout = trim(strsep(&rest , "<"));
-//             trim(rest);
-//             fin = rest;
-//         }
-//         // < >
-//         if ( !first && sin == NULL && sout!=NULL){
-//             fin = strsep(&rest , ">");
-//             trim(rest);
-//             fout = rest;
+    while ( (sin=strchr(rest, '<')) != NULL 
+      ||  (sout=strchr(rest, '>')) != NULL ) {     
+      //contains "<" or ">" 
+        if (first && sin != NULL && sout==NULL){
+        // only input
+            bufadd = strsep(&rest , "<");
+            trim(rest);
+            fin = rest;
+        }
+        if (first && sin == NULL && sout!=NULL){
+        // only output
+            bufadd = strsep(&rest , ">");
+            trim(rest);
+            fout = rest;
+        }
+        if (sin != NULL && sout!=NULL)
+            bufadd = strsep(&rest , "<>");         
+        if (!first && sin != NULL && sout==NULL){
+        // > < 
+            fout = trim(strsep(&rest , "<"));
+            trim(rest);
+            fin = rest;
+        }
+        // < >
+        if ( !first && sin == NULL && sout!=NULL){
+            fin = strsep(&rest , ">");
+            trim(rest);
+            fout = rest;
 
-//         }
-//         first = 0;
+        }
+        first = 0;
 
-//        if (fin != NULL) {
-//             fdin = open(fin, O_RDONLY);
-//        }
-//        if (fout != NULL) {
-//             fdout = open(fout, O_CREAT, 0666);
-//             close(fdout);
-//             fdout = open(fout, O_WRONLY);
-//        }       
-//     } 
+       if (fin != NULL) {
+            fdin = open(fin, O_RDONLY);
+       }
+       if (fout != NULL) {
+            fdout = open(fout, O_CREAT, 0666);
+            close(fdout);
+            fdout = open(fout, O_WRONLY);
+       }       
+    } 
     
-//     trim(bufadd);
-//   }
-// }
+    trim(bufadd);
+  }
+}
